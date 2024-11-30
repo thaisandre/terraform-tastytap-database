c6c74ca9ec71407b386d4bc2e6d5071f1134a3cf
thaisandre <thaisandre.sc@gmail.com>
2024-11-30 02:10:29 -0300

    fix tf files + add workflow

--- database/main.tf
+++ database/main.tf
@@ -88,7 +88,7 @@ resource "aws_db_instance" "tastytap" {
 }
 
 resource "aws_db_instance" "users" {
-  identifier             = "tastytap_users"
+  identifier             = "tastytap-users"
   allocated_storage      = 10
   instance_class         = "db.t3.micro"
   storage_type           = "gp2"
@@ -105,4 +105,38 @@ resource "aws_db_instance" "users" {
   tags = {
     Name = "tastytap_users"
   }
+}
+
+provider "mongodbatlas" {
+  public_key  = var.mongodbatlas_public_key
+  private_key = var.mongodbatlas_private_key
+}
+
+resource "mongodbatlas_database_user" "db-user" {
+  auth_database_name = var.mongo_db_username
+  username           = var.mongo_db_username
+  password           = var.mongo_db_password
+  project_id         = var.project_id
+
+  roles {
+    role_name     = "readWrite"
+    database_name = "tastytap"
+    collection_name = "payments"
+  }
+}
+
+resource "mongodbatlas_cluster" "tastytap_payment" {
+  project_id                  = var.project_id
+  name                        = "mongodb-tastytap-cluster"
+  cluster_type                = "REPLICASET"
+  
+  provider_name               = "TENANT"
+  backing_provider_name       = "AWS"
+  provider_region_name        = "US_EAST_1"
+  provider_instance_size_name = "M0"
+}
+
+resource "mongodbatlas_project_ip_access_list" "ip_access_list" {
+  project_id = var.project_id
+  cidr_block = var.atlas_cluster_allow_inbound_from_cidr
 }
